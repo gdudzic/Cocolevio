af07921ebd91cdd367dd176315c0266a09f63174
gdudzic <31224568+gdudzic@users.noreply.github.com>
2017-08-22 16:52:41 -0500

    Price Per Unit
    It makes a lot more sense to calculate the price that each company is paying per unit to see who pays more for one unit of material. This way it accounts for scenarios with limited amount of material, maximizing the money received by the selling company.

---
+++ redo.py
@@ -0,0 +1,29 @@
+
+
+
+def main():
+	#Companies [company, amount, price]
+	buyers = [['Company A',1,1],['Company B',2,5],['Company C',3,8],['Company D',4,9],['Company E',5,10],['Company F',6,17],['Company G',7,17],['Company H',8,20],['Company I',9,24],['Company',10,30]] #Data
+	sellTo = {} #final list ranked by most profitable 
+
+	track = 0
+	for x in buyers:
+		name = buyers[track][0]
+		amount = buyers[track][1]
+		price = buyers[track][2]
+		ppunit = price/amount #price per unit
+		sale = price * amount
+
+		#add company price per units to a dictionary
+		sellTo[name] = ppunit
+		track += 1
+	print(sellTo)
+
+		
+
+
+
+
+
+
+main()
