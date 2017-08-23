32c492dd09810ccc9e55ff5faebe15292b45bc7e
gdudzic <31224568+gdudzic@users.noreply.github.com>
2017-08-23 14:57:46 -0500

    Take in data from a separate file
    This program was made assuming that the table/list of companies to sell to is not information that exists within the program initially. The program reads the data from a separate file (that the user can direct it to) and puts it into a list which then goes through the program, producing a ranked list of the buying companies from most profitable to least profitable.

--- cocolevio.py
+++ cocolevio.py
@@ -1,25 +1,61 @@
+def listCompanies(group):
+	#Companies [company, amount, price]
+	#program reads in data from txt doc for companies instead of beginning with that info
+	buyers = []
+	for line in group:
+		line = line.strip() #No more \n woohoo! 
+		line = line.split(' ')
+		sub = []
+		for item in line:
+			sub.append(item)
+		buyers.append(sub)
+	return(buyers)
+
 def pricePer(customers):
 	buyers = customers
 	#Calculate the price per unit that each company is buying for
 	sellTo = {}
-	track = 0
+	track = 1
 	for x in buyers:
-		name = buyers[track][0]
-		amount = buyers[track][1]
-		price = buyers[track][2]
+		name = buyers[0][track]
+		amount = int(buyers[1][track])
+		price = int(buyers[2][track])
 		ppunit = price/amount #price per unit
 		sale = price * amount
 
 		#add company price per units to a dictionary
 		sellTo[name] = ppunit
 		track += 1
+		#break
 	return(sellTo)
 
 def main():
-	#Companies [company, amount, price]
-	buyers = [['Company A',1,1],['Company B',2,5],['Company C',3,8],['Company D',4,9],['Company E',5,10],['Company F',6,17],['Company G',7,17],['Company H',8,20],['Company I',9,24],['Company J',10,30]] #Data
+
+	#Get list of sample data buyer companies
+	possComp = input("Enter name of data sheet: ")
+	buyerss = "./{}".format(possComp)
+	buyertxt = open(buyerss, "r")
+	buyers = listCompanies(buyertxt)
+	buyertxt.close()
+	#print(buyers)
+
+	#Calculate price at which each company is buying material per unit 
 	ppunit = pricePer(buyers)
-	sortList = [(k,v) for v,k in sorted([(v,k) for k,v in ppunit.items()], reverse = True)]
-	print(sortList) #Got help from http://bytesizebio.net/2013/04/03/stupid-python-tricks-3296-sorting-a-dictionary-by-its-values/  Stupid Python Tricks 
+
+	#Give me a list filtered by price per unit (high to low)
+	sortList = [(k,v) for v,k in sorted([(v,k) for k,v in ppunit.items()], reverse = True)] #Sorting by values (price per unit)
+	#Got help from http://bytesizebio.net/2013/04/03/stupid-python-tricks-3296-sorting-a-dictionary-by-its-values/  Stupid Python Tricks 
+	track = 0
+	for i in sortList: #This loop prints a prettier (relatively speaking :( )) list 
+		print(sortList[track])
+		track += 1
 
 main()
+
+
+
+
+
+
+
+
